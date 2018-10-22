bf574e412a59a075620cfce74a22f8e69610ccf9
Concept 11 Studios <concept11studios@gmail.com>
2018-10-22 13:57:51 -0400

    Index work

--- JS/index.js
+++ JS/index.js
@@ -3,3 +3,7 @@ $(".jumbotron").css({ height: $(window).height() + "px" });
 $(window).on("resize", function () {
     $(".jumbotron").css({ height: $(window).height() + "px" });
 });
+
+$(window).scroll(function(){
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
